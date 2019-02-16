718f469fdb80b5d1bf7479c719d1f5bcdae7e2e7
Youssouf <yssouare@gmail.com>
2019-02-16 20:14:00 +0100

    Another essai !!

--- src/essaigithub/EssaiGithub.java
+++ src/essaigithub/EssaiGithub.java
@@ -15,7 +15,9 @@ public class EssaiGithub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Github !!");
+        
+        String great ="Hello World !! ";
+        System.out.println(great);
         // TODO code application logic here
     }
     
